33e1237e4a37d3d7ccf3e39f4242743f1e3ae4b6
vitor-gercov <gercovvitor@gmail.com>
2026-03-18 08:25:26 -0300

    melhoria de componentização dos shapes

--- src/app/components/shape/shape.component.ts
+++ src/app/components/shape/shape.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Shape } from '../../models';
 import { NgClass } from '@angular/common';
-import { SectionTitleDirective } from 'app/components/core/ui/section-title/section-title.directive';
+import { CardComponent } from 'app/components/core/ui/card/card.component';
 
 @Component({
   selector: 'app-shape',
-  imports: [NgClass, SectionTitleDirective],
+  imports: [NgClass, CardComponent],
   templateUrl: './shape.component.html',
   styleUrl: './shape.component.css'
 })
