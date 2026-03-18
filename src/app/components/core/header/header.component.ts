cd5d370475ed3360a07a041f0ca81f966780fc75
vitor-gercov <gercovvitor@gmail.com>
2026-03-18 13:29:57 -0300

    inclusão de logo do site

--- src/app/components/core/header/header.component.ts
+++ src/app/components/core/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { ThemeService } from '../../../services/theme.service';
 import { NotesHelpModalComponent } from './notes-help-modal/notes-help-modal.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [NotesHelpModalComponent],
+  imports: [NotesHelpModalComponent, NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
